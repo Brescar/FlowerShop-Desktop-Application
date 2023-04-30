8033f5272cc4e7b9416d6fd15e727991247487ad
Brescar <ghitavalentin2001@gmail.com>
2023-04-30 21:45:21 +0300

    reminders as comments/notes

--- Program.cs
+++ Program.cs
@@ -21,19 +21,12 @@ namespace FlowerShop
             /*THINGS THAT NEED ADDING / MODIFYING
              * 1) To add the condition where adding or editing a product where 2 instances
              * of the same item appear, somehow the stock is combined into only one item
-             * 2) to add implementation of the statusStrip 
-             * 3) started implementing forms and such for orders
-             * 4) to add implementation for StockOrder, StockDelivery and adapt Stock
-             * to StockFlower;
-             * 5) to adapt the code when addingor editing happens, entrys in StockName
-             * needs to be updated accordingly
-             * 6) to implement clear (total delete) and restore (per item and per all items)
+            *@ 2) !!!! to add implementation of the statusStrip 
+             * 3) to implement clear (total delete) and restore (per item and per all items)
              * as in Undo
-             * 7) to implement shortcuts
+             * 4) to implement clickColumn => sorting
              * 
-             * !!! ) immediate to do:  left to add for delivery : menustrip for delivery, 
-             * contextstrip for delivery, and implementations for buttons and veryfying 
-             * and code in form1
+             * 
              */
         }
     }
